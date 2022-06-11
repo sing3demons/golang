185df1fc41c5100579c868057e84bff051035f2b
sing3demons <sing3demons@live.com>
2022-06-12 05:46:44 +0700

    test: edit k6 script

--- goredis/scripts/test.js
+++ goredis/scripts/test.js
@@ -1,10 +1,10 @@
 import http from "k6/http";
 
 export let options = {
-    vus: 10,
-    duration: "10s",
+    vus: 5,
+    duration: "5s",
 };
 
 export default function() {
-    http.get("http://host.docker.internal:8080/hello");
+    http.get("http://host.docker.internal:8080/products");
 }
