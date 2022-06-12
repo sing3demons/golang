914b2e502dee86a894bc1613988fcf07e41838f3
sing3demons <sing3demons@live.com>
2022-06-12 07:16:52 +0700

    test: /services/grade_test.go  coverage: 100.0% of statements

--- gotest/services/grade_test.go
+++ gotest/services/grade_test.go
@@ -7,10 +7,30 @@ import (
 )
 
 func TestCheckGrade(t *testing.T) {
-	grade := services.CheckGrade(81)
-	expected := "A"
-
-	if grade != expected {
-		t.Errorf("got %v expected %v", grade, expected)
+	type testCase struct {
+		name     string
+		score    int
+		expected string
 	}
+
+	cases := []testCase{
+		{name: "success grade A", score: 85, expected: "A"},
+		{name: "success grade B", score: 75, expected: "B"},
+		{name: "success grade C", score: 66, expected: "C"},
+		{name: "success grade D", score: 59, expected: "D"},
+		{name: "success grade F", score: 49, expected: "F"},
+	}
+
+	for _, c := range cases {
+
+		t.Run(c.name, func(t *testing.T) {
+			grade := services.CheckGrade(c.score)
+			expected := c.expected
+
+			if grade != expected {
+				t.Errorf("got %v expected %v", grade, expected)
+			}
+		})
+	}
+
 }
